d9cbf2a39d704c13e239d1196ce03cd2993f078e
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-05-08 11:10:10 -0400

    Update config.js

--- VideoCode/config.js
+++ VideoCode/config.js
@@ -5,7 +5,7 @@ const DB_URI = (process.env.NODE_ENV === "test")
   : "postgresql:///express_auth";
 
 const SECRET_KEY = process.env.SECRET_KEY || "fHt$TRcC%25HmE8gC'vD$!Ka%8sOt;~V,XItr+8t+)8;x}O/rSQgGyG7}o^c1')"
-
+//bycrpt work factor 
 const BCRYPT_WORK_FACTOR = 12;
 
 module.exports = {
